e4f7d05d1fa17e7c398df683bb7ee046b903ab7f
higoomigo <hilalbouti57@gmail.com>
2024-12-18 17:08:25 +0800

    add update kata

--- resources/views/layout/appAdmin.blade.php
+++ resources/views/layout/appAdmin.blade.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-<nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+<nav class="fixed top-0 z-50 w-full bg-gradient-to-r from-purple-500 to-purple-700 border-b border-gray-200 ">
   <div class="px-3 py-3 lg:px-5 lg:pl-3">
     <div class="flex items-center justify-between">
       <div class="flex items-center justify-start rtl:justify-end">
