a467c336e68942887f8c4007e513668658ba4759
higoomigo <hilalbouti57@gmail.com>
2024-12-24 01:02:33 +0800

    fix tailwind.config vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ import plugin from 'flowbite/plugin';
 export default {
   darkMode: 'media',
   content: [
-    './resources/views/**/*.blade.php',  // For Blade templates
-    './resources/js/**/*.js',           // For JavaScript files
+    './resources/views/**/*.blade.php',  // Blade templates
+    './resources/js/**/*.js',           // JavaScript files
     './resources/css/**/*.css',
-    './node_modules/flowbite/**/*.js'
+    './node_modules/flowbite/**/*.js',
   ],
   theme: {
     extend: {},
