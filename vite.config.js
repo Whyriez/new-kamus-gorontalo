a1d38db8203da7f834b1138ffc99c1e7f04f1820
higoomigo <hilalbouti57@gmail.com>
2024-12-24 12:26:15 +0800

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel from 'vite-plugin-laravel';
 
 export default defineConfig({
-    base: '/build/',
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'public/build',  // Pastikan output file berada di folder yang benar
+    manifest: true,
+    rollupOptions: {
+      input: {
+        app: 'resources/css/app.css',
+        js: 'resources/js/app.js',
+      },
+    },
+  },
 });
