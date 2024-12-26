863e29a9bba6ccf422a3da7985849af1eb1429ab
Alim Suma <sumaalim@gmail.com>
2024-12-26 14:08:54 +0800

    asas

--- api/index.php
+++ api/index.php
@@ -1,11 +1,4 @@
-<?php
+<?php 
 
-require 'vendor/autoload.php';
 
-use Illuminate\Http\Request;
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$request = Request::capture();
-$response = $app->handle($request);
-$response->send();
+require __DIR__ . "/../public/index.php";
