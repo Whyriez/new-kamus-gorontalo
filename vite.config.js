864adce014ec606474af21722b558c6237d7a101
Alim Suma <sumaalim@gmail.com>
2024-12-26 13:29:05 +0800

    trs

--- vite.config.js
+++ vite.config.js
@@ -6,22 +6,8 @@ import ViteLaravel from 'vite-plugin-laravel'; // Laravel plugin for Vite
 export default defineConfig({
   plugins: [
     ViteLaravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'], // Configure input files for Vite
-      refresh: true, // Automatic refresh on file changes
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+        refresh: true,
     }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'resources/js'), // Alias for JS directory
-    },
-  },
-  server: {
-    https: true, // Run the Vite server with HTTPS during development
-    host: '0.0.0.0', // Allow access from any IP address
-    port: 3000, // Port for the development server
-  },
-  build: {
-    outDir: 'public/build', // Output directory for Laravel build files
-    manifest: true, // Enable manifest for Laravel's vite() helper
-  },
+],
 });
